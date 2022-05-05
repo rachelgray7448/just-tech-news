79ea347e9b0b6f832814912bb612c2dd799ea292
Rachel Gray <rachel.gray7448@gmail.com>
2022-05-04 22:04:07 -0400

    fixed the bug for the comment functionality

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,15 +2,13 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 
 router.get('/', (req, res) => {
-    Comment.findAll({
-        attributes: ['id', 'comment_text', 'user_id', 'post_id'],
-    })
+    Comment.findAll()
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
-    });
-});
+        });
+}); 
 
 router.post('/', (req, res) => {
     Comment.create({
