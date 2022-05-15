38bbd19fa8e2eaf5394e19f8d2bc9149043f7edc
Rachel Gray <rachel.gray7448@gmail.com>
2022-05-15 13:23:27 -0400

    added partials for plurals, date format, url format

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const helpers = require('./utils/helpers');
 const session = require("express-session");
 const SequlizeStore = require("connect-session-sequelize")(session.Store);
 const path = require("path");
@@ -5,7 +6,7 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
